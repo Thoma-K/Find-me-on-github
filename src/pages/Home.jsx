4a287eb3e22dc148b2e0064de845e30d1460eebe
Thomas <96667415+Thoma-K@users.noreply.github.com>
2022-08-28 17:30:13 +0200

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,20 +7,10 @@ const Home = () => {
     document.title = "Find Me On Github | Home";  
   }, []);
   return (
-  <>
-    {/* <script>
-      window.dataLayer = window.dataLayer || [];
-      window.dataLayer.push({
-        event: 'virtualPageview',
-        pageURL: window.location.href,
-        pageTitle: document.title
-      })
-    </script> */}
     <div>
       <UserSearch />
       <UserResults />
     </div>
-  </>
   )
 }
 
